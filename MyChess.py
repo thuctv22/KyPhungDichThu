3395f6787b7276174aad8ac0378ecb1221be8d8d
Truong Van Thuc <thuctv22@gmail.com>
2022-10-06 14:51:17 +0700

    change

--- MyChess.py
+++ MyChess.py
@@ -290,7 +290,7 @@ while True:
 
         elif event.type == KEYDOWN:
             if event.key == K_SPACE:
-                print("you pressed SPACE")
+                print("you pressed SPACE, delete row and column")
                 Red_Rook_chess = cbd.find_Red_Rook()
                 col_num = Red_Rook_chess.col_num
                 row_num = Red_Rook_chess.row_num
@@ -300,8 +300,6 @@ while True:
                 cbd.delete_chessman_in_a_column(col_num)
                 delete_sprite_in_a_column(col_num, row_num)
                 cbd.switch_turn_to_black()
-<<<<<<< HEAD
-=======
             if event.key == K_r:
                 print("you pressed R, delete current row")
                 Red_Rook_chess = cbd.find_Red_Rook()
@@ -330,12 +328,6 @@ while True:
                     cbd.switch_turn_to_black()
                 else:
                     cbd.switch_turn_to_red()
->>>>>>> 72a17699ff08e14d343b0a7b9bf241289d4ebd4e
-
-
-
-                
-
 
         framerate.tick(20)
 
@@ -347,74 +339,3 @@ while True:
         cbd.clear_all_chessman_killing_list(screen)
         cbd.print_all_chessman_killing_list(screen)
         pygame.display.update()
-
-# def main(winstyle=0):
-
-#     pygame.init()
-#     bestdepth = pygame.display.mode_ok(SCREENRECT.size, winstyle, 32)
-#     screen = pygame.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
-#     pygame.display.set_caption("中国象棋最强AI")
-
-#     # create the background, tile the bgd image
-#     bgdtile = load_image('boardchess.gif')
-#     background = pygame.Surface(SCREENRECT.size)
-#     for x in range(0, SCREENRECT.width, bgdtile.get_width()):
-#         background.blit(bgdtile, (x, 0))
-#     screen.blit(background, (0, 0))
-#     pygame.display.flip()
-
-#     cbd = Chessboard.Chessboard('000')
-#     cbd.init_board()
-
-#     chessmans = pygame.sprite.Group()
-#     framerate = pygame.time.Clock()
-
-#     creat_sprite_group(chessmans, cbd.chessmans_hash)
-#     current_chessman = None
-#     cbd.calc_chessmans_moving_list()
-#     while not cbd.is_end():
-#     #     for event in pygame.event.get():
-#     #         if event.type == pygame.QUIT:
-#     #             sys.exit()
-#     #         elif event.type == MOUSEBUTTONDOWN:
-#     #             pressed_array = pygame.mouse.get_pressed()
-#     #             for index in range(len(pressed_array)):
-#     #                 if index == 0 and pressed_array[index]:
-#     #                     mouse_x, mouse_y = pygame.mouse.get_pos()
-#     #                     col_num, row_num = translate_hit_area(mouse_x, mouse_y)
-#     #                     chessman_sprite = select_sprite_from_group(
-#     #                         chessmans, col_num, row_num)
-#     #                     if current_chessman is None and chessman_sprite != None:
-#     #                         if chessman_sprite.chessman.is_red == cbd.is_red_turn:
-#     #                             current_chessman = chessman_sprite
-#     #                             chessman_sprite.is_selected = True
-#     #                     elif current_chessman != None and chessman_sprite != None:
-#     #                         if chessman_sprite.chessman.is_red == cbd.is_red_turn:
-#     #                             current_chessman.is_selected = False
-#     #                             current_chessman = chessman_sprite
-#     #                             chessman_sprite.is_selected = True
-#     #                         else:
-#     #                             success = current_chessman.move(
-#     #                                 col_num, row_num)
-#     #                             if success:
-#     #                                 chessmans.remove(chessman_sprite)
-#     #                                 chessman_sprite.kill()
-#     #                                 current_chessman.is_selected = False
-#     #                                 current_chessman = None
-#     #                     elif current_chessman != None and chessman_sprite is None:
-#     #                         success = current_chessman.move(col_num, row_num)
-#     #                         if success:
-#     #                             current_chessman.is_selected = False
-#     #                             current_chessman = None
-#         framerate.tick(20)
-#         # clear/erase the last drawn sprites
-#         chessmans.clear(screen, background)
-
-#         # update all the sprites
-#         chessmans.update()
-#         chessmans.draw(screen)
-#         pygame.display.update()
-
-
-# if __name__ == '__main__':
-#     main()
